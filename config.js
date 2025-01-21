5e888aaa30b48db9f6abee503f29deeb80f747b5
KD STUDIO <kd1076347@gmail.com>
2025-01-21 16:00:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Remove this and add your session ID here",
-MONGODB: process.env.MONGODB || "Remove this and add your MongoDB public URL here",
-OWNER_NUM: process.env.OWNER_NUM || "94704101989", // Remove this number and add your number with country code
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~w2lWiLrL#F958VUBlKIVv-9Djf_xulePuJom52Re4rMMD2omEY6g",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ivScDiAEjcmzndSaksqYPMtULZXQdfej@autorack.proxy.rlwy.net:54307",
+OWNER_NUM: process.env.OWNER_NUM || "94720552487", // Remove this number and add your number with country code
 };
+  
